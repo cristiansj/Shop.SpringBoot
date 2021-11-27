70ae186f4d5a0483240233c7a6441111ffcd2f97
unknown <cristian.solartej@uqvirtual.edu.co>
2021-11-27 15:18:04 -0500

    Se realizaron test para comprobar los servicios

--- negocio/src/test/java/co/edu/uniquindio/proyecto/test/ProductoServicioTest.java
+++ negocio/src/test/java/co/edu/uniquindio/proyecto/test/ProductoServicioTest.java
@@ -1,4 +1,43 @@
 package co.edu.uniquindio.proyecto.test;
 
+import co.edu.uniquindio.proyecto.NegocioApplication;
+import co.edu.uniquindio.proyecto.entidades.Producto;
+import co.edu.uniquindio.proyecto.entidades.Usuario;
+import co.edu.uniquindio.proyecto.servicios.ProductoServicioImplementation;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+
+@SpringBootTest(classes = NegocioApplication.class)
+@Transactional
 public class ProductoServicioTest {
+
+    @Autowired
+    private ProductoServicioImplementation productoServicioImplementation;
+
+    @Test
+    public void publicarProductoTest(){
+        Producto producto = new Producto();
+        try {
+            Producto productoPublicado = productoServicioImplementation.publicarProducto(producto);
+            Assertions.assertNotNull(productoPublicado);
+        }catch (Exception e){
+
+        }
+    }
+
+    @Test
+    public void actualizarProductoTest(){
+        Usuario usuario = new Usuario(123,"Cristian","cristian20","cristian@gmail.com","123",)
+        Producto producto = new Producto(123,"camisa",10,"camisa buena","camisa","hahshhsa",1235.0, LocalDateTime.now(),);
+        try {
+            Producto productoPublicado = productoServicioImplementation.publicarProducto(producto);
+            Assertions.assertNotNull(productoPublicado);
+        }catch (Exception e){
+
+        }
+    }
 }
