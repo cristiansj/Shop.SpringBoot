e9b22b4c7b2865626ab75ad976131b866ca5d884
unknown <cristian.solartej@uqvirtual.edu.co>
2021-11-28 03:11:11 -0500

    Se creo el proyecto web inicial

---
+++ Web/src/main/java/co/edu/uniquindio/proyecto/bean/InicioBean.java
@@ -0,0 +1,17 @@
+package co.edu.uniquindio.proyecto.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+import javax.faces.view.ViewScoped;
+import java.io.Serializable;
+
+@Component
+@ViewScoped
+public class InicioBean implements Serializable {
+
+    @Getter
+    @Setter
+    private String mensaje = "mi primera encarta";
+}
